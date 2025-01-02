be32bfa200aeacd019d5008814c4c06cb4e843e4
Ayodeji Akinola <akinayodeji4all@gmail.com>
2025-01-02 21:07:46 +0100

    fix failing test

--- gossipod/src/ip_addr.rs
+++ gossipod/src/ip_addr.rs
@@ -84,7 +84,7 @@ mod tests {
     #[test]
     fn test_from_ipaddr() {
         let ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-        let ip_address = ip.into();
+        let ip_address = IpAddress::from(ip);
         assert_eq!(ip_address.0, ip);
     }
 
